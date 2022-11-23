2ebc35a3b6222597a24fa0d50b0e2a57e77dddbe
HassibKareme <96119570+HassibKareme@users.noreply.github.com>
2022-11-23 13:57:52 +0100

    fix

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request =require ('supertest');
 
 describe('get campus', () => {
     it('should return first campus', async () => {
-        await request('http://campus-api-example.netlify.app') //Dit is de api van de docent. Je kan die van jezelf ook gebruiken.
+        await request('https://campus-api-example.netlify.app') //Dit is de api van de docent. Je kan die van jezelf ook gebruiken.
         .get('/.netlify/functions/api/campus')
         .expect(200) //request geslaagd opvragen
         .expect('Content-Type', 'application/json; charset=utf-8')
