c87d83cbe4b2d71ab5e76b4841c1f4a6dd8fef1c
HassibKareme <96119570+HassibKareme@users.noreply.github.com>
2022-11-23 14:57:12 +0100

    fix

--- test/test.js
+++ test/test.js
@@ -1,29 +1,51 @@
+// const request = require('supertest');
+// const fs = require('fs');
+// const {
+//     json
+// } = require('express');
+
+// describe('get campus', () => {
+//     it('should return first campus', async () => {
+//         await request('https://campus-api-example.netlify.app') //Dit is de api van de docent. Je kan die van jezelf ook gebruiken.
+//             .get('/.netlify/functions/api/campus')
+//             .expect(200) //request geslaagd opvragen
+//             .expect('Content-Type', 'application/json; charset=utf-8')
+//             .expect((res) => {
+
+//                 fs.writeFile("output.json"), JSON.stringify(res.body[0]), 'utf8',
+//                     function (err) {
+//                         if (err) {
+//                             console.log("An error occured while writing JSON Object to File.");
+//                             return console.log(err);
+//                         }
+
+//                         console.log("Json file has been saved.");
+//                     }
+//                     console.log(res.body[0]) //We verwachte de eerste campus terug.
+//                 })
+//     })
+// })
+
+// //Json file uploaden naar 
+
 const request = require('supertest');
 const fs = require('fs');
-const {
-    json
-} = require('express');
 
 describe('get campus', () => {
     it('should return first campus', async () => {
-        await request('https://campus-api-example.netlify.app') //Dit is de api van de docent. Je kan die van jezelf ook gebruiken.
-            .get('/.netlify/functions/api/campus')
-            .expect(200) //request geslaagd opvragen
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect((res) => {
-
-                fs.writeFile("output.json"), JSON.stringify(res.body[0]), 'utf8',
-                    function (err) {
-                        if (err) {
-                            console.log("An error occured while writing JSON Object to File.");
-                            return console.log(err);
-                        }
-
-                        console.log("Json file has been saved.");
-                    }
-                    console.log(res.body[0]) //We verwachte de eerste campus terug.
-                })
-    })
-})
-
-//Json file uploaden naar 
+        await request('https://campus-api-example.netlify.app')
+        .get('/.netlify/functions/api/campus')
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+            console.log(res.body[0])
+            fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function (err) {
+                if (err) {
+                    console.log("An error occured while writing JSON Object to file.");
+                    return console.log(err);
+                }
+                console.log("JSON file has been saved.")
+            })
+        })
+    });
+});
